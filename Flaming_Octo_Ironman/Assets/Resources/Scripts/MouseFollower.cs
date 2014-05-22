6cb4414dda6638c53027766b3b26ab54a65e8b37
Michael Christensen-Calvin <mrcc912@gmail.com>
2014-05-21 23:43:37 -0700

    FUCK THE MAGIC SCHOOLBUS

---
+++ Flaming_Octo_Ironman/Assets/Resources/Scripts/MouseFollower.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollower : MonoBehaviour {
+
+	private float movementScaler = 5;
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = pos;
+	}
+}
