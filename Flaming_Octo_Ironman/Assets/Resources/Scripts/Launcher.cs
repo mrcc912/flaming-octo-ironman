2e271cbb5182cd49f2631c5950c8e3f2c1438a55
Michael Christensen-Calvin <mrcc912@gmail.com>
2014-05-21 19:37:55 -0700

    Adding Projectile Trap

---
+++ Flaming_Octo_Ironman/Assets/Resources/Scripts/Launcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour {
+
+	[SerializeField] private GameObject projectile;
+	[SerializeField] private Transform spawnLocation;
+	[SerializeField] private float repeatRate = 0.5f;
+
+
+	void Start()
+	{
+		InvokeRepeating("launchProjectile", 0, repeatRate);
+	}
+
+	private void launchProjectile()
+	{
+		Instantiate(projectile, spawnLocation.position, Quaternion.identity);
+	}
+}
---
+++ Flaming_Octo_Ironman/Assets/Resources/Scripts/Projectile.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour {
+
+	[SerializeField] private Direction direction = Direction.DOWN;
+	[SerializeField] private float xMagnitude;
+	[SerializeField] private float yMagnitude;
+
+	private Rigidbody2D rigidBody;
+
+
+	void Awake()
+	{
+		Vector2 movementVector = Vector2.zero;
+		switch(direction)
+		{
+		case Direction.UP:
+			movementVector = new Vector2(0, yMagnitude);
+			break;
+		case Direction.DOWN:
+			movementVector = new Vector2(0, -yMagnitude);
+			break;
+		case Direction.RIGHT:
+			movementVector = new Vector2(xMagnitude, 0);
+			break;
+		case Direction.LEFT:
+			movementVector = new Vector2(-xMagnitude, 0);
+			break;
+			
+		}
+		rigidBody = GetComponent<Rigidbody2D>();
+		rigidbody2D.velocity = movementVector;
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+
+	private enum Direction
+	{
+		UP, DOWN, LEFT, RIGHT
+	}
+
+	void OnCollisionEnter2D(Collision2D c)
+	{
+		Destroy(this.gameObject);
+	}
+}
