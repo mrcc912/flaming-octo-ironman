4c3a806673decdb88957af8d0de66f55369b0d21
Michael Christensen-Calvin <mrcc912@gmail.com>
2014-05-22 00:34:47 -0700

    What do you do with a drunken sailor?

--- Flaming_Octo_Ironman/Assets/Resources/Scripts/Invisible.cs
+++ Flaming_Octo_Ironman/Assets/Resources/Scripts/Invisible.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class Invisible : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private SpriteRenderer r;
+
+	void Awake()
+	{
+		r = GetComponentInChildren<SpriteRenderer>();
+
+		Color c = r.color;
+		c.a = 0;
+		r.color = c;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void SetVisible(bool visible)
+	{
+		Color c = r.color;
+		c.a = visible ? 1.0f : 0f;
+		r.color = c;
+		Debug.Log ("Color is now" + r.color.a);
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		Debug.Log("trigger enter!");
+		if(c.GetComponent<MagicLens>())
+			SetVisible(true);
+	}
+
+	void OnTriggerExit2D(Collider2D c)
+	{
+		Debug.Log("trigger exit!");
+		if(c.GetComponent<MagicLens>())
+			SetVisible(false);
 	}
 }
