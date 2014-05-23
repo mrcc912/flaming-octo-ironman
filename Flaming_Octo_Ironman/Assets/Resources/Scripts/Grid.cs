128bc4ac174932bbae01d290d7554956eb83e2b1
Michael Christensen-Calvin <mrcc912@gmail.com>
2014-05-23 02:49:17 -0700

    I NEED TO FUCKING SLEEP ASSHOLES

---
+++ Flaming_Octo_Ironman/Assets/Resources/Scripts/Grid.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class Grid : MonoBehaviour {
+
+#if UNITY_EDITOR
+	public bool refreshNow = false;
+#endif
+	public float cellWidth = 2.0f;
+	public float cellHeight = 2.0f;
+	public Direction direction;
+	public bool centerElements = false;
+	public bool ignoreScale = false;
+	public List<GameObject> elements;
+
+	private Vector3 originalPosition;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		originalPosition = transform.position;
+		elements = new List<GameObject>();
+		Refresh();
+	}
+
+#if UNITY_EDITOR
+	void Update()
+	{
+		if(refreshNow)
+		{
+			Refresh();
+			refreshNow = false;
+		}
+	}
+#endif
+
+	public void Refresh()
+	{
+		elements.Clear();
+		int count = 0;
+		foreach(Transform child in transform)
+		{
+			child.localPosition = Vector3.zero; // resetting to the center of the grid.
+			Vector3 toPosition = child.position;
+			if(direction == Direction.Horizontal)
+			{
+				toPosition.x += cellWidth * count;
+			}
+			else
+			{
+				toPosition.y += cellHeight * count;
+			}
+			child.position = toPosition;
+			elements.Add(child.gameObject);
+			++count;
+		}
+		if(centerElements)
+		{
+			foreach(Transform child in transform)
+			{
+				Vector3 toPosition = child.position;
+				if(direction == Direction.Horizontal)
+				{
+					toPosition.x -= ((count-1) * (cellWidth/2));
+				}
+				else
+				{
+					toPosition.y -= ((count-1) * (cellHeight/2));
+				}
+				child.position = toPosition;
+			}
+
+		}
+	}
+
+	public enum Direction
+	{
+		Horizontal,
+		Vertical
+	}
+}
---
+++ Flaming_Octo_Ironman/Assets/Resources/Scripts/TileSetup.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Grid))]
+public class TileSetup : MonoBehaviour {
+
+	public bool update = false;
+	public bool murderChildren = false;
+	public GameObject tilePrefab;
+
+
+	void Awake()
+	{
+		if(transform.localScale != Vector3.one)
+		{
+			BoxCollider2D box = GetComponent<BoxCollider2D>();
+			box.size = new Vector2(box.size.x * transform.localScale.x, box.size.y * transform.localScale.y);
+			transform.localScale = Vector3.one;
+			DestroyImmediate(GetComponent<SpriteRenderer>());
+		}
+	}
+	
+	void Update()
+	{
+		if(murderChildren)
+		{
+			List<GameObject> children = new List<GameObject>();
+
+			foreach(Transform child in transform)
+			{
+				children.Add(child.gameObject);
+			}
+
+			foreach(GameObject babySeal in children)
+			{
+				DestroyImmediate(babySeal);
+			}
+			murderChildren = false;
+		}
+
+		if(update)
+		{
+			Setup();
+			update = false;
+		}
+	}
+
+	void Setup()
+	{
+		GameObject go = (GameObject)Instantiate(tilePrefab, Vector3.zero, Quaternion.identity);
+		Grid grid = GetComponent<Grid>();
+		BoxCollider2D box = GetComponent<BoxCollider2D>();
+		go.transform.parent = transform;	
+		Vector3 scale;
+		Grid.Direction dir;
+		bool at90Degrees = false;
+		if( Mathf.Abs ( Mathf.Abs(transform.localRotation.z) - 0.7f ) < 0.1f)
+		{
+			at90Degrees = true;
+		}
+
+		if(box.size.y > box.size.x)
+		{
+			if(at90Degrees)
+			{
+				grid.direction = Grid.Direction.Horizontal;
+			}
+			else
+			{
+				grid.direction = Grid.Direction.Vertical;
+			}
+			scale = new Vector3(box.size.x * 3.2f, box.size.x * 3.2f, go.transform.localScale.z);
+			grid.cellWidth = box.size.x;
+			grid.cellHeight = box.size.x;
+			go.transform.localScale = scale;
+
+			for(int i=1; i<(box.size.y / grid.cellHeight); i++)
+			{
+				GameObject tile = (GameObject)Instantiate(tilePrefab, Vector3.zero, Quaternion.identity);
+				tile.transform.parent = transform;
+				tile.transform.localScale = scale;
+			}
+
+		}
+		else
+		{
+			if(at90Degrees)
+			{
+				grid.direction = Grid.Direction.Vertical;
+			}
+			else
+			{
+				grid.direction = Grid.Direction.Horizontal;
+			}
+			scale = new Vector3(box.size.y * 3.2f, box.size.y * 3.2f, go.transform.localScale.z);
+			grid.cellWidth = box.size.y;
+			grid.cellHeight = box.size.y;
+			go.transform.localScale = scale;
+			for(int i=1; i<(box.size.x / grid.cellWidth); i++)
+			{
+				GameObject tile = (GameObject)Instantiate(tilePrefab, Vector3.zero, Quaternion.identity);
+				tile.transform.parent = transform;
+				tile.transform.localScale = scale;
+			}
+
+		}
+		grid.centerElements = true;
+		grid.Refresh();
+	}
+}
