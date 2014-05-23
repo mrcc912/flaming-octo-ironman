e2466a7af0d54f30aa843fe2112ba46d75c14eb9
Joseph Domenici <josephdomenici@gmail.com>
2014-05-22 23:57:25 -0700

    Updated checkpoints, broke the shooting PIU PIU PIU

--- Flaming_Octo_Ironman/Assets/Resources/Scripts/TargetedShot.cs
+++ Flaming_Octo_Ironman/Assets/Resources/Scripts/TargetedShot.cs
@@ -6,6 +6,8 @@ public class TargetedShot : MonoBehaviour {
 	public GameObject projectile;
 	public bool canShoot;
 
+	private GameObject shotToTrack = null;
+
 	void Awake()
 	{
 		canShoot = true;
@@ -13,13 +15,26 @@ public class TargetedShot : MonoBehaviour {
 
 	void Update()
 	{
+		if (shotToTrack != null)
+		{
+			canShoot = false;
+		}
+		else
+		{
+			canShoot = true;
+		}
+
 		if (Input.GetMouseButtonUp(0) && canShoot)
 		{
-			Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-			Quaternion rotation = Quaternion.FromToRotation(Vector2.up, direction);
+			{
+				Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+				Quaternion rotation = Quaternion.FromToRotation(Vector2.up, direction);
 
-			GameObject shot = (GameObject)Instantiate(projectile, transform.position, rotation);
-			shot.GetComponent<TargetedProjectile>().Launch(direction);
+				GameObject shot = (GameObject)Instantiate(projectile, transform.position, rotation);
+				shot.GetComponent<TargetedProjectile>().Launch(direction);
+
+				shotToTrack = shot;
+			}
 		}
 	}
 }
